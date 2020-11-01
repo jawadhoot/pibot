8ee7fbc4a11c85e64b738d1818f49d44aa762c88
Awadhoot Jog <jawadhoot@gmail.com>
2020-11-01 20:47:27 +0530

    add score dict

--- cricbuzz.py
+++ cricbuzz.py
@@ -3,16 +3,36 @@ from requests import get
 
 url = 'https://m.cricbuzz.com'
 response = get(url)
+MATCHHEADER_SPLIT = '\xa0•&nbsp'
+SCORE_SPLIT = ' '
 
 def extractor():
-	responses = []
+	scores = []
 	html_soup = BeautifulSoup(response.text, 'html.parser')
-	matches = html_soup.find_all('a', class_ = 'cb-list-item')
-	for match in matches[:2]:
-		result = []
-		result.append(match.find('span', class_ = 'bat-team-scores').text)
-		result.append(match.find('span', class_ = 'bat-team-scores-test').text)
-		result.append(match.find('span', class_ = 'cbz-ui-home').text)
-		responses.append(';'.join(result))
-	return '\n'.join(responses)	
+	match_list = html_soup.find('div',class_ = 'list-group')
+	matches = match_list.find_all('a', class_ = 'cb-list-item')
+	for match in matches:
+		result = {}
+		matchheader = match.find('div', class_ = 'matchheader').text.split(MATCHHEADER_SPLIT)
+		team1 = match.find('span', class_ = 'bat-team-scores').text
+		team2 = match.find('span', class_ = 'bat-team-scores-test').text
+		summary = match.find('span', class_ = 'cbz-ui-home').text
+		
+		result['tournament'] = matchheader[1]
+		result['tornament_match'] = matchheader[0]
+		result['teams'] = [team1.split(SCORE_SPLIT)[0],team2.split(SCORE_SPLIT)[0]] 
+		result['team1-score'] = team1
+		result['team2-score'] = team2
+		result['summary'] = summary
 
+		scores.append(result)
+	return scores
+
+def filter():
+	pass
+
+def xmpp_transform():
+	pass
+
+if __name__ == "__main__":
+	print(extractor())
